9786205be69ed05ff6e6ce861b4fb45c042450ca
Brandon M. Keltz <keltzbm@icloud.com>
2023-02-09 11:46:56 -0700

    Update Node.c

--- C/Node.c
+++ C/Node.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node createNode(int data) {
-	struct Node node;
-	node.data = data;
-	node.next = NULL;
+Node* createNode(int data) {
+	Node* node;
+	node->data = data;
+	node->next = NULL;
 	return node;
 };
 
 int main() {
-	struct Node test = createNode(9);
-	printf("test = %d.\n", test.data);
+	Node* test = createNode(9);
+	printf("test = %d.\n", test->data);
 	return 0;
 };
